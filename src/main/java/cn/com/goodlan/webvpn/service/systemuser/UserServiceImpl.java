2bbe7d9bea8d19d69c9343bfa14782faa837ac6a
liukai <zhizhufan@foxmail.com>
2022-11-08 10:02:30 +0800

    redis

--- src/main/java/cn/com/goodlan/webvpn/service/systemuser/UserServiceImpl.java
+++ src/main/java/cn/com/goodlan/webvpn/service/systemuser/UserServiceImpl.java
@@ -84,6 +84,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public void update(UserDTO userDTO) {
         SystemUser user = userRepository.getById(userDTO.getId());
+        user.updateName(userDTO.getName());
 //        user.updateRemark(userDTO.getRemark());
 //        user.updateSex(userDTO.getSex());
 //        user.updateEmail(userDTO.getEmail());
