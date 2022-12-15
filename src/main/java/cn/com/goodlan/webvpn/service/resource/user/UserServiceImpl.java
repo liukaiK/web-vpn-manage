0b0333622e8928c669f0c660444beb4106b019fb
liukai <zhizhufan@foxmail.com>
2022-12-15 16:39:44 +0800

    同步缓存

--- src/main/java/cn/com/goodlan/webvpn/service/resource/user/UserServiceImpl.java
+++ src/main/java/cn/com/goodlan/webvpn/service/resource/user/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.com.goodlan.webvpn.service.resource.user;
 
+import cn.com.goodlan.webvpn.exception.BusinessException;
 import cn.com.goodlan.webvpn.mapstruct.UserMapper;
 import cn.com.goodlan.webvpn.pojo.dto.UserDTO;
 import cn.com.goodlan.webvpn.pojo.entity.resource.role.ResourceRole;
@@ -8,19 +9,22 @@ import cn.com.goodlan.webvpn.pojo.vo.UserVO;
 import cn.com.goodlan.webvpn.repository.resource.role.ResourceRoleRepository;
 import cn.com.goodlan.webvpn.repository.resource.user.UserRepository;
 import cn.hutool.core.convert.Convert;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.criteria.Predicate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author liukai
@@ -29,11 +33,21 @@ import java.util.List;
 @Transactional(rollbackFor = Exception.class)
 public class UserServiceImpl implements UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private ResourceRoleRepository resourceRoleRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    private final String userCacheKey = "user";
 
     @Override
     public Page<UserVO> search(UserDTO userDTO, Pageable pageable) {
@@ -60,6 +74,25 @@ public class UserServiceImpl implements UserService {
 
         User user = new User(userDTO.getName(), userDTO.getUsername(), roles);
         userRepository.save(user);
+
+        putUserInCache(user);
+
+    }
+
+
+    private void putUserInCache(User user) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", user.getId());
+        map.put("name", user.getName());
+        map.put("username", user.getUsername());
+        map.put("roleIds", user.getRoleIds());
+        map.put("roleNames", user.getRoleNames());
+        try {
+            redisTemplate.opsForHash().put(userCacheKey, user.getUsername(), objectMapper.writeValueAsString(map));
+        } catch (JsonProcessingException e) {
+            log.error("序列化json失败", e);
+            throw new BusinessException("用户保存到缓存失败");
+        }
     }
 
     @Override
@@ -72,6 +105,8 @@ public class UserServiceImpl implements UserService {
 
         user.updateRoles(roles);
         userRepository.save(user);
+
+        putUserInCache(user);
     }
 
     @Override
